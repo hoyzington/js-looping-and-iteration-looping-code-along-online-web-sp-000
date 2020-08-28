e7b60ce75b4f56511c657839210c6e36a4e93376
Scott Hoisington <hoyzington@gmail.com>
2020-08-28 22:29:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const names = ['Ada', 'Brendan', 'Ali'];
 
 function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
-    console.log(``);
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
 }
+
+
